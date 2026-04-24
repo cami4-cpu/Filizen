cce4c603121fdf44743ed2810068002ad251a1c8
cami4-cpu <camilofiligrana9@gmail.com>
2026-04-24 15:48:47 -0500

    Update response message with rocket emoji

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend funcionando");
+  res.send("Backend funcionando 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
