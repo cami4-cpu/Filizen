ad7727dca0e62e4ef18ca00ea398feb31136d737
cami4-cpu <camilofiligrana9@gmail.com>
2026-04-24 17:43:44 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB conectado"))
+.catch(err => console.log(err));
 
 app.use(cors());
 app.use(express.json());
