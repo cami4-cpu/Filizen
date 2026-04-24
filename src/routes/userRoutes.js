442d7c79187ffbd2d0391dfbeffdef964f67f92f
cami4-cpu <camilofiligrana9@gmail.com>
2026-04-24 17:05:01 -0500

    Delete src/routes directory

--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ mensaje: "Ruta usuarios funcionando" });
-});
-
-module.exports = router;
